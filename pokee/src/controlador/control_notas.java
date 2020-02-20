69c4837c19543cf0c29aa7731ff95a2faa9ae429
bryan-padilla1999 <59964656+bryan-padilla1999@users.noreply.github.com>
2020-02-19 23:25:18 -0500

    Prueba2Pokemon

---
+++ pokee/src/controlador/control_notas.java
@@ -0,0 +1,38 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+
+/**
+ *
+ * @author ANDRES
+ */
+public class control_notas {
+    
+      private sql sen;
+    
+    public control_notas()
+    {
+      sen = new sql();  
+    }
+    
+    
+    
+    public Object[][] consulta_materia_notas(String id)
+       {
+        String[] columnas={"cod_carnet","cod_materia", "nombrepokemon"};
+        Object[][] resultado = sen.datosTabla(columnas, "entrepoke", "select * from entrepoke, pokemon where cod_materia=id  and cod_carnet='"+id+"';");
+        return resultado;
+       }
+    
+    public boolean ingresar_notas(String cod_carnetnota, String cod_materianota, Double nota)
+    {               
+           
+            String datos[] = {cod_carnetnota, cod_materianota, Double.toString(nota)};           
+            return sen.insertar(datos, "insert into nota(cod_carnetnota, cod_materianota, nota) values(?,?,?)");
+                                  
+        
+    }
+    
+}
